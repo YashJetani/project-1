5ff3613e0d99a0fe9c15086449fd600b7f52aff8
Yash Jetani <yashjetani123@gmail.com>
2026-02-10 16:02:29 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ from .routers import todos
 from .database import engine, Base
 from .config import settings
 
-
+@app.get("/")
+def root():
+    return {
+        "message": "Todo API is running! 🚀 Visit /api/v1/docs for interactive docs"
+    }
 app = FastAPI(title=settings.PROJECT_NAME)
 
 
